0678b7753eac72f51e9ed6270e6da0a179737024
SON Contractor Dev <dev@soncontractor.com>
2026-07-28 07:50:48 +0700

    Fix: Add cache buster to iframe src to bypass browser cache

--- src/components/StoreDragDropView.jsx
+++ src/components/StoreDragDropView.jsx
@@ -7,7 +7,7 @@ export default function StoreDragDropView() {
     <div className="module-iframe-wrap">
       <iframe
         ref={iframeRef}
-        src="/apps/store-dragdrop/index.html"
+        src="/apps/store-dragdrop/index.html?v=1.1"
         title="Store Drag & Drop System"
         className=""
         scrolling="yes"
